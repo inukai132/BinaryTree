1d3141a50f0de16da95235df988fd2bd38361752
Tylor Childers <tylord1@gmail.com>
2016-03-24 18:59:05 -0400

    Finish Problem1
    PostOrder, InOrder, and PreOrder

--- Binary Tree/problem1.cpp
+++ Binary Tree/problem1.cpp
@@ -14,7 +14,7 @@ class BinTree
         	};
     	BinTree(int _size)
     	{
-    	    height = ceil(log2(_size));
+    	    int height = ceil(log2(_size));
     	    size = pow(2,height);
     		tree = new Node*[size];
     	}
@@ -29,11 +29,45 @@ class BinTree
     	   if(_idx < 0 || _idx >= size)
     	        return;
     	        
-    	   tree[_idx] = new Node{_data, _left, _right};    	    
+    	   Node* n = new Node();
+    	   n->data = _data;
+    	   n->left = _left;
+    	   n->right = _right;
+    	   
+    	   tree[_idx] = n;	    
     	}
     	string traverse()
     	{
-    	    return "";
+    	    cout << postOrder("", tree[0]) << endl;
+    	    cout << inOrder("", tree[0]) << endl;
+    	    cout << preOrder("", tree[0]) << endl;
+    	}
+    	string postOrder(string cur, Node* root)
+    	{
+    	    if(root->left != -1)
+    	        cur = postOrder(cur, tree[root->left]);
+    	    if(root->right != -1)
+    	        cur = postOrder(cur, tree[root->right]);
+    	    cur = cur + root->data + " ";
+    	    return cur;
+    	}
+    	string inOrder(string cur, Node* root)
+    	{
+    	    if(root->left != -1)
+    	        cur = inOrder(cur, tree[root->left]);
+    	    cur = cur + root->data + " ";
+    	    if(root->right != -1)
+    	        cur = inOrder(cur, tree[root->right]);
+    	    return cur;
+    	}
+    	string preOrder(string cur, Node* root)
+    	{
+    	    cur = cur + root->data + " ";
+    	    if(root->left != -1)
+    	        cur = preOrder(cur, tree[root->left]);
+    	    if(root->right != -1)
+    	        cur = preOrder(cur, tree[root->right]);
+    	    return cur;
     	}
     private:
         Node** tree;
@@ -52,6 +86,6 @@ int main()
         cin >> data >> left >> right;
         tree->Add(i, data, left, right);
     }
-    cout << tree->traverse();
+    tree->traverse();
     return 0;
 }
